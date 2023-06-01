bb9d7c457ae9ec94b0c18faebb240b6b2786133a
Rohan Reddy <125650486+darkneptune69@users.noreply.github.com>
2023-06-01 15:54:40 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -4,5 +4,3 @@ print("Hello world")
 print("Update: This update will reflect once Jenkins auto builds after 5 minutes.")
 
 print("Update 2: Testing once more, just to be sure.")
-
-print("changessssssssssssssssssssssssssss are made by rohan")
